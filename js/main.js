76e58d2933cf0587d5cbf8106c635ac2a44dee54
michael de groot <michaeldegroot@michaelootsimac.home>
2017-04-26 20:45:20 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
  console.log("Up and running!");
- var cardOne = 'queen';
- var cardTwo = 'queen';
- var cardThree = 'king';
- var cardFour = 'king';
 
- console.log('User flipped ' + cardOne);
- console.log('User flipped ' + cardThree);
+ var cards = ['queen','queen','king','king'];
+ var cardsInPlay = [];
+ var cardOne = cards[0];
+ var cardTwo = cards[1];
+ cardsInPlay.push('cardOne');
+ cardsInPlay.push('cardTwo');
+
+ console.log('User flipped queen');
+ console.log('user flipped king');
+
+ if (cardsInPlay.length === 2) {
+ 	if (cardsInPlay[0] === cardsInPlay[1]) {
+alert('match!')
+ } else {
+ 	alert('No match!')
+ }};
