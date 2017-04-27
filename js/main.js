595791fcde5e210116c6a1c8bd7e7bd35e06780a
michael de groot <michaeldegroot@michaelootsimac.home>
2017-04-27 16:55:50 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,14 +1,38 @@
  console.log("Up and running!");
 
- var cards = ['queen','queen','king','king'];
+ var cards = [
+ {
+ 	rank: 'queen',
+ 	suit: 'hearts',
+ 	cardImage: "images/queen-of-hearts.png"
+ },
+ {
+ 	rank: 'queen',
+ 	suit: 'diamonds',
+ 	cardImage: "images/queen-of-diamonds.png"
+ },
+ {
+ 	rank: 'king',
+ 	suit: 'hearts',
+ 	cardImage: "images/king-of-hearts.png"
+ },
+ {
+ 	rank: 'king',
+ 	suit: 'diamonds',
+ 	cardImage: "images/king-of-diamonds.png"
+ }
+ ];
  var cardsInPlay = [];
 
+//Checks for the cards matching IF there are 2 in play
 var checkForMatch = function () {
 	if (cardsInPlay.length === 2) {
 	  if (cardsInPlay[0] === cardsInPlay[1]) {
       console.log("You found a match!");
+      alert("You found a match!");
   } else {
       console.log("Sorry, try again.");
+      alert("Sorry, try again.");
   }
 };
 };
@@ -16,10 +40,11 @@ var checkForMatch = function () {
 
 var flipCard = function (cardID){
 
- console.log('User flipped ' + cards[cardID])
+ console.log('User flipped ' + cards[cardID].rank);
+ console.log(cards[cardID].cardImage);
 
 // This line pushes the card ID to the cards in play array
- cardsInPlay.push(cards[cardID]);
+ cardsInPlay.push(cards[cardID].rank);
  checkForMatch();
 };
 
